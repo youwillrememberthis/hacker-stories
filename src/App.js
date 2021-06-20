9b047c2a20ff84ea10d23ce77b81d93678cfe1da
Jeigh Rizzuhh <designthirsty@gmail.com>
2021-06-20 01:52:47 -0400

    testing ignore 062021

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import * as React from 'react';
+
+
+
+const App = () => {
+  const stories = [
+    {
+      title: 'React test test',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 2,
+      points: 9,
+      objectID: 0,
+    },
+    {
+      title: 'Redux test test',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 8,
+      points: 4,
+      objectID: 1,
+    },
+  ]
+
+  return (
+    <div>
+      <h1>Hacker Stories testing ignore</h1>
+      
+      <Search />
+
+      <hr />
+      
+      <List list={stories} />
+
+    </div>
+  );
+}
+
+const Search = () => {
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <div>
+      <label htmlFor="search">Search</label>
+      <input id="search" type="text" onChange={handleChange} />
+
+      <p>
+        Searching for <strong>{searchTerm}</strong>.
+      </p>
+    </div>
+  );
+}
+
+const List = (props) => (
+    <ul>
+        {props.list.map((item) => (
+            <Item key={item.objectID} item={item} />
+        ))}
+      </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span>{props.item.author}</span>
+    <span>{props.item.num_comments}</span>
+    <span>{props.item.points}</span>
+  </li>
+);
+export default App;
