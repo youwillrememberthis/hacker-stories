d04f6ad056494da712425f8736a1a9a8c7981f93
Jeigh Rizzuhh <designthirsty@gmail.com>
2021-06-20 06:30:46 -0400

    testing ignore 062021_02

--- src/App.js
+++ src/App.js
@@ -3,30 +3,42 @@ import * as React from 'react';
 
 
 const App = () => {
+  //console.log('App renders');
+
+  
+
   const stories = [
     {
       title: 'React test test',
       url: 'https://reactjs.org/',
       author: 'Jordan Walke',
-      num_comments: 3,
-      points: 7,
+      num_comments: 2,
+      points: 9,
       objectID: 0,
     },
     {
       title: 'Redux test test',
       url: 'https://redux.js.org/',
       author: 'Dan Abramov, Andrew Clark',
-      num_comments: 4,
-      points: 8,
+      num_comments: 8,
+      points: 4,
       objectID: 1,
     },
-  ]
+  ];
+
+  // A
+  const handleSearch = (event) => {
+    // C
+    console.log(event.target.value);
+  };
+
+  
 
   return (
     <div>
-      <h1>My Hacker Stories</h1>
+      <h1>Hacker Stories testing ignore</h1>
       
-      <Search />
+      <Search onSearch={handleSearch} />
 
       <hr />
       
@@ -36,16 +48,21 @@ const App = () => {
   );
 }
 
-const Search = () => {
+const Search = (props) => {
+  //console.log("Search renders");
+  
   const [searchTerm, setSearchTerm] = React.useState('');
 
   const handleChange = (event) => {
     setSearchTerm(event.target.value);
+
+    //B
+    props.onSearch(event);
   };
 
   return (
     <div>
-      <label htmlFor="search">Search</label>
+      <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange} />
 
       <p>
@@ -56,11 +73,12 @@ const Search = () => {
 }
 
 const List = (props) => (
+    
     <ul>
         {props.list.map((item) => (
             <Item key={item.objectID} item={item} />
         ))}
-      </ul>
+    </ul>
 );
 
 const Item = (props) => (
